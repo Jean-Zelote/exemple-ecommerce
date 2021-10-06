abeec723c984c0a03d80fae84286bb889f31ad0b
davmaene <davidmened@gmail.com>
2021-10-06 12:30:29 +0200

    up line

--- index.php
+++ index.php
@@ -57,11 +57,7 @@
     </header>
     <div class="container-fluid mt-3">
         <div class="col-12">
-            <div class="row products-content">
-                <div class="col-lg-12 my-5 py-5 d-flex justify-content-center bg-danger-">
-                    <img src="assets/imgs/loader_gif.gif" alt="Loader" width="200" style="mix-blend-mode: multiply;">
-                </div>
-            </div>
+            <?php include("./pages/$page") ?>
         </div>
     </div>
 
