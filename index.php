2e7b728ba2966a082cb31a31bc545964c878f755
davmaene <davidmened@gmail.com>
2021-10-06 12:26:03 +0200

    up line

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
         <div class="col-12">
             <div class="row products-content">
                 <div class="col-lg-12 my-5 py-5 d-flex justify-content-center bg-danger-">
-                    <img src="assets/imgs/loader_gif.gif" alt="Loader" srcset="">
+                    <img src="assets/imgs/loader_gif.gif" alt="Loader" width="200" style="mix-blend-mode: multiply;">
                 </div>
             </div>
         </div>
